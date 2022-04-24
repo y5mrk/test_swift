854eef7a1a7c4000fa2913f2cb3560572b829e11
y5mrk <kzm326.ag@gmail.com>
2022-04-25 01:51:32 +0900

    Initial Commit

---
+++ test_twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_twitter
+//
+//  Created by YoshimuraKazumi on 2022/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_twitter/test_twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_twitterApp.swift
+//  test_twitter
+//
+//  Created by YoshimuraKazumi on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct test_twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
